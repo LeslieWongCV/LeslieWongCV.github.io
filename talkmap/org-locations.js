5b36d8a89ba8fbe00240c41e82965bc177cb9f78
LeslieWongCV <yushuowang@outlook.com>
2019-11-08 23:53:58 +0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,26 +1,26 @@
 var addressPoints = [
   [
-    "Miami ,USA | Electronic and Computer Engineering Department, University of Miami | <a herf='https://lesliewongcv.github.io/posts/2018/08/blog-post-1'>2018 Summer Research Program</a>",
+    "Miami ,USA | Electronic and Computer Engineering Department, University of Miami | <a href='https://lesliewongcv.github.io/posts/2018/08/blog-post-1'>2018 Summer Research Program</a>",
     25.7192,
     -80.2771
   ],
   [
-    "City Analytics Lab, UNSW, AU | <a herf='https://lesliewongcv.github.io/posts/2018/11/blog-post-1'>Lab and Campus Visiting at UNSW</a>",
+    "City Analytics Lab, UNSW, AU | <a href='https://lesliewongcv.github.io/posts/2018/11/blog-post-1'>Lab and Campus Visiting at UNSW</a>",
     -33.9173,
     151.2313
   ],
   [
-    "London, UK | <a herf='https://lesliewongcv.github.io/posts/2019/03/blog-post-1'>Lab and Campus Visiting</a>",
+    "London, UK | <a href='https://lesliewongcv.github.io/posts/2019/03/blog-post-1'>Lab and Campus Visiting</a>",
     51.5074, 
     -0.1278
   ],
   [
-    "Los Angeles, CA | <a herf'https://lesliewongcv.github.io/posts/2018/07/blog-post-2'>Santa Monica</a>",
+    "Los Angeles, CA | <a href'https://lesliewongcv.github.io/posts/2018/07/blog-post-2'>Santa Monica</a>",
     34.0195, 
     -118.4912
   ],
   [
-    "Seoul, KR | International Conference of Computer Vision, 2019 | <a herf='https://lesliewongcv.github.io/posts/2019/11/blog-post-1'>The 2nd Large-scale Video Object Segmentation Challenge</a>",
+    "Seoul, KR | International Conference of Computer Vision, 2019 | <a href='https://lesliewongcv.github.io/posts/2019/11/blog-post-1'>The 2nd Large-scale Video Object Segmentation Challenge</a>",
     37.5665, 
     126.9780
   ],
