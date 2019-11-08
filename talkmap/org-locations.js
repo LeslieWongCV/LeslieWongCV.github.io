496e874db82ed789a791d72d3d163e7b3ad289f4
LeslieWongCV <yushuowang@outlook.com>
2019-11-08 21:19:42 +0800

    talkmap loca.js fixed

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,7 +18,7 @@ var addressPoints = [
     "Los Angeles, CA",
     34.0543942,
     -118.2439408
-  ]
+  ],
   [
     "Seoul, KR | International Conference of Computer Vision, 2019 | <a herf='https://lesliewongcv.github.io/posts/2019/11/blog-post-1/'>The 2nd Large-scale Video Object Segmentation Challenge</a>",
     37.5665, 
